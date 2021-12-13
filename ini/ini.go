160d2d045bf9962caecacf2134e7757fefb226f1
ws <wangshang@nj.iscas.ac.cn>
2021-12-13 09:11:25 +0800

    add commit

--- ini/ini.go
+++ ini/ini.go
@@ -10,6 +10,7 @@ type Ini struct {
 	Cfg  *ini.File
 }
 
+// New 创建对象
 func New(path string) *Ini {
 	return &Ini{
 		Path: path,
@@ -17,6 +18,7 @@ func New(path string) *Ini {
 	}
 }
 
+// Add 在内存中的指定Section下添加key和value(注意：此操作仅在内存中进行，调用Save方法后保存进文件)
 func (i *Ini) Add(section, key, val string) {
 	if !i.Cfg.HasSection(section) {
 		_, err := i.Cfg.NewSection(section)
@@ -37,15 +39,18 @@ func (i *Ini) Add(section, key, val string) {
 	}
 }
 
+// Set 指定Section下，若key存在，则修改；若key不存在，则创建
 func (i *Ini) Set(section, key, val string) {
 	i.Cfg.Section(section).Key(key).SetValue(val)
 	i.Save()
 }
 
+// Get 获取指定Section下key所对应的value值
 func (i *Ini) Get(section, key string) string {
 	return i.Cfg.Section(section).Key(key).String()
 }
 
+// Save 将内存中的ini结构存入文件
 func (i *Ini) Save() {
 	err := i.Cfg.SaveTo(i.Path)
 	if err != nil {
@@ -53,6 +58,7 @@ func (i *Ini) Save() {
 	}
 }
 
+// Marshal 读取文件映射为既定结构
 func (i *Ini) Marshal(v interface{}) bool {
 	err := i.Cfg.MapTo(v)
 	if err != nil {
@@ -62,6 +68,7 @@ func (i *Ini) Marshal(v interface{}) bool {
 	return true
 }
 
+// Unmarshal 数据结构保存进文件
 func (i *Ini) Unmarshal(v interface{}) bool {
 	err := ini.ReflectFrom(i.Cfg, v)
 	if err != nil {
