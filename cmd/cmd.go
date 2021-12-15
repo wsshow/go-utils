d67b25f39e1a81be2368151cdc8a580c94203746
ws <wangshang@nj.iscas.ac.cn>
2021-12-15 16:07:14 +0800

    add function cmd.Find

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 	"runtime"
@@ -13,10 +14,11 @@ func logLn(v ...interface{}) {
 	log.Println(logTitle, v)
 }
 
+var curOS = runtime.GOOS
+
 func Run(cmd string) string {
 	var result []byte
 	var err error
-	curOS := runtime.GOOS
 	if curOS == "linux" {
 		result, err = exec.Command("/bin/sh", "-c", cmd).Output()
 	} else if curOS == "windows" {
@@ -26,4 +28,14 @@ func Run(cmd string) string {
 		logLn("cmd:", cmd, "error:", err)
 	}
 	return strings.TrimSpace(string(result))
+}
+
+func Find(s string) string {
+	var rt string
+	if curOS == "linux" {
+		rt = Run(fmt.Sprintf("find / -name \"%s\" 2>/dev/null", s))
+	} else if curOS == "windows" {
+		rt = Run(fmt.Sprintf("powershell ls / -Recurse -Include \"%s\"", s))
+	}
+	return rt
 }
--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -1,7 +1,14 @@
 package cmd
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestRun(t *testing.T) {
 	logLn(Run("powershell ps -Name 'Sys*'"))
+}
+
+func TestFind(t *testing.T) {
+	s := Find("agent-collector*")
+	logLn(s)
 }
