cb1d1956839349a5f82dfab5a7d7af25e789cab9
ws <wangshang@nj.iscas.ac.cn>
2022-05-18 09:57:45 +0800

    更新注释

--- bitOp/bitOp.go
+++ bitOp/bitOp.go
@@ -4,10 +4,13 @@ import "log"
 
 // 位运算，组合属性
 const (
-	Ldate = 1 << iota
-	Ltime
-	Lmicriseconds
+	Ldate         = 1 << iota //0000 0001
+	Ltime                     //0000 0010
+	Lmicriseconds             //0000 0100
 )
+
+// Ldate | Ltime => 0000 0011 => 3
+// (Ldate | Ltime) & Ldate => (0000 0011) & (0000 0001) => (0000 0001) => Ldate => 1
 
 func FormatHeader(flag int) {
 	if (flag & Ldate) != 0 {
